4ef50ea0280117db1fbb947441c87461d16e1b87
Brian Field <brian@madsalty.com>
2015-08-22 21:09:12 -0700

    Add test for Action Creators

--- app/test/reducers/squidbuilds.spec.js
+++ app/test/reducers/squidbuilds.spec.js
@@ -1,4 +1,4 @@
-import assert from 'assert'
+import assert from 'assert';
 import Immutable, { Map, List } from 'immutable';
 import squidbuilds from '../../reducers/squidbuilds';
 import * as types from '../../constants/ActionTypes';
